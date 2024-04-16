966bd678541f64408f260a371be1b2a29d9cc662
Drak <matt@plas.host>
2024-04-15 23:00:56 -0400

    Minor issues.

--- src/main/java/io/streamlined/bukkit/instances/RBaseRunnable.java
+++ src/main/java/io/streamlined/bukkit/instances/RBaseRunnable.java
@@ -2,18 +2,30 @@ package io.streamlined.bukkit.instances;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.bukkit.Location;
+import tv.quaint.objects.SingleSet;
 
+import java.util.function.Function;
+
+@Setter
+@Getter
 public class RBaseRunnable extends BaseRunnable {
-    @Getter @Setter
     private Runnable runnable;
+    private Function<Void, SingleSet<Location, Runnable>> foliaGetter;
 
-    public RBaseRunnable(Runnable runnable, int delay, int period, boolean isAsyncable) {
+    public RBaseRunnable(Runnable runnable, Function<Void, SingleSet<Location, Runnable>> foliaGetter, int delay, int period, boolean isAsyncable) {
         super(delay, period, isAsyncable);
         this.runnable = runnable;
+        this.foliaGetter = foliaGetter;
     }
 
     @Override
     public void execute() {
         runnable.run();
+    }
+
+    @Override
+    public SingleSet<Location, Runnable> executeWhenFolia() {
+        return foliaGetter.apply(null);
     }
 }
--- src/main/java/io/streamlined/bukkit/instances/RDelayedRunnable.java
+++ src/main/java/io/streamlined/bukkit/instances/RDelayedRunnable.java
@@ -2,18 +2,30 @@ package io.streamlined.bukkit.instances;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.bukkit.Location;
+import tv.quaint.objects.SingleSet;
 
+import java.util.function.Function;
+
+@Setter
+@Getter
 public class RDelayedRunnable extends DelayedRunnable {
-    @Getter @Setter
     private Runnable runnable;
+    private Function<Void, SingleSet<Location, Runnable>> foliaGetter;
 
-    public RDelayedRunnable(Runnable runnable, int delay, boolean isAsyncable) {
+    public RDelayedRunnable(Runnable runnable, Function<Void, SingleSet<Location, Runnable>> foliaGetter, int delay, boolean isAsyncable) {
         super(delay, isAsyncable);
         this.runnable = runnable;
+        this.foliaGetter = foliaGetter;
     }
 
     @Override
     public void onlyOnce() {
         runnable.run();
+    }
+
+    @Override
+    public SingleSet<Location, Runnable> executeWhenFolia() {
+        return foliaGetter.apply(null);
     }
 }
