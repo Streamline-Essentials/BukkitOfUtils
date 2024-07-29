dd215ba6284051ec5dbdf86d072c27a1e3e6db90
nitra <matt@plas.host>
2024-07-29 16:46:53 -0400

    Fixed lag caused by forName() method.

--- src/main/java/host/plas/bou/utils/ClassHelper.java
+++ src/main/java/host/plas/bou/utils/ClassHelper.java
@@ -1,13 +1,28 @@
 package host.plas.bou.utils;
 
+import lombok.Getter;
+
+import java.util.concurrent.ConcurrentSkipListMap;
+
 public class ClassHelper {
-    public static boolean hasClass(String fullClass) {
+    public static final ConcurrentSkipListMap<String, Boolean> PROPERTY_CACHE = new ConcurrentSkipListMap<>();
+
+    public static boolean hasClassNoCache(String fullClass) {
         try {
             Class.forName(fullClass);
             return true;
         } catch (ClassNotFoundException e) {
             return false;
         }
+    }
+
+    public static boolean hasClass(String fullClass) {
+        Boolean cached = PROPERTY_CACHE.get(fullClass);
+        if (cached != null) return cached;
+
+        boolean hasClass = hasClassNoCache(fullClass);
+        PROPERTY_CACHE.put(fullClass, hasClass);
+        return hasClass;
     }
 
     public static boolean isFolia() {
